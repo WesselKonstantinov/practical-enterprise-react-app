c029c9b969258ddaf30e6099ca9ecae648e932d4
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-10-16 13:33:12 +0200

    Create 404 not found page

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,13 +1,16 @@
 import React, { lazy, Suspense } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import AboutPage from './views/pages/AboutPage';
 import Home from './views/pages/Home';
+import NotFoundPage from './views/pages/NotFoundPage';
 
 const Routes = () => {
 	return (
 		<Switch>
 			<Route exact path="/" component={Home} />
 			<Route exact path="/about" component={AboutPage} />
+			<Route exact path="/not-found" component={NotFoundPage} />
+			<Redirect from={'*'} to={'/not-found'} />
 		</Switch>
 	);
 };
