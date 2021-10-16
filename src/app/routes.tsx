27641a7c6c142c80ca42b1d0b114c9dd095329a7
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-10-16 15:43:45 +0200

    Enable lazy loading

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,39 +1,53 @@
 import React, { lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import AboutPage from './views/pages/AboutPage';
+import LinearProgress from '@mui/material/LinearProgress';
 import Home from './views/pages/Home';
 import NotFoundPage from './views/pages/NotFoundPage';
 import Dashboard from './layouts/dashboard-layout';
-import DashboardDefaultContent from './views/dashboard/dashboard-default-content';
-import SettingsAndPrivacy from './views/dashboard/settings-and-privacy';
 
 const Routes = () => {
 	return (
-		<Switch>
-			<Route exact path="/" component={Home} />
-			<Route exact path="/about" component={AboutPage} />
-			<Route exact path="/not-found" component={NotFoundPage} />
-			<Route
-				path={'/dashboard'}
-				render={({ match: { path } }) => (
-					<Dashboard>
-						<Switch>
-							<Route
-								exact
-								path={path + '/'}
-								component={DashboardDefaultContent}
-							/>
-							<Route
-								exact
-								path={path + '/settings-and-privacy'}
-								component={SettingsAndPrivacy}
-							/>
-						</Switch>
-					</Dashboard>
-				)}
-			/>
-			<Redirect from={'*'} to={'/not-found'} />
-		</Switch>
+		<Suspense fallback={<LinearProgress style={{ margin: '10rem' }} />}>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route
+					exact
+					path="/about"
+					component={lazy(() => import('./views/pages/AboutPage'))}
+				/>
+				<Route exact path="/not-found" component={NotFoundPage} />
+				<Route
+					path={'/dashboard'}
+					render={({ match: { path } }) => (
+						<Dashboard>
+							<Switch>
+								<Route
+									exact
+									path={path + '/'}
+									component={lazy(
+										() =>
+											import(
+												'./views/dashboard/dashboard-default-content'
+											),
+									)}
+								/>
+								<Route
+									exact
+									path={path + '/settings-and-privacy'}
+									component={lazy(
+										() =>
+											import(
+												'./views/dashboard/settings-and-privacy'
+											),
+									)}
+								/>
+							</Switch>
+						</Dashboard>
+					)}
+				/>
+				<Redirect from={'*'} to={'/not-found'} />
+			</Switch>
+		</Suspense>
 	);
 };
 
