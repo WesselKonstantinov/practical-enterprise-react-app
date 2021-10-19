47dd3a19c1a2ac4e99fce66e536f90ed4c3df29c
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-10-19 23:19:35 +0200

    Remove warnings

--- src/index.tsx
+++ src/index.tsx
@@ -31,21 +31,19 @@ const store = configureAppStore();
 const MOUNT_NODE = document.getElementById('root') as HTMLElement;
 
 ReactDOM.render(
-  <Provider store={store}>
-    <HelmetProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </HelmetProvider>
-  </Provider>,
-  MOUNT_NODE,
+	<Provider store={store}>
+		<HelmetProvider>
+			<App />
+		</HelmetProvider>
+	</Provider>,
+	MOUNT_NODE,
 );
 
 // Hot reloadable translation json files
 if (module.hot) {
-  module.hot.accept(['./locales/i18n'], () => {
-    // No need to render the App again because i18next works with the hooks
-  });
+	module.hot.accept(['./locales/i18n'], () => {
+		// No need to render the App again because i18next works with the hooks
+	});
 }
 
 // If you want to start measuring performance in your app, pass a function
