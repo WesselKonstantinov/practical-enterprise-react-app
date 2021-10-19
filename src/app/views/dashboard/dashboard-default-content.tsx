d09ccb370d85a827099d5a4beeba0d4ee916b68c
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-10-19 21:56:24 +0200

    Fetch data inside dashboard component

--- src/app/views/dashboard/dashboard-default-content.tsx
+++ src/app/views/dashboard/dashboard-default-content.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getSalesAxios } from 'services/saleService';
+import { SaleType } from 'models/sale-type';
 
 const DashboardDefaultContent = () => {
+	const [sales, setSales] = useState<SaleType[]>([]);
+
+	useEffect(() => {
+		fetchSales();
+	}, []);
+
+	const fetchSales = async () => {
+		const { data } = await getSalesAxios();
+		console.log(data);
+		setSales(data);
+	};
+
 	return (
 		<div>
 			<h1>Dashboard Default Content</h1>
+			<h2>{sales.length}</h2>
 		</div>
 	);
 };
