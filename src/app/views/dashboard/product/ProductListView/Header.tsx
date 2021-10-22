7106d61b04d3aa50bd6e9f4d4cc46ade2168c1c1
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-10-22 21:02:35 +0200

    Complete ProductListView

--- src/app/views/dashboard/product/ProductListView/Header.tsx
+++ src/app/views/dashboard/product/ProductListView/Header.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import clsx from 'clsx';
+import { NavigateNext } from '@mui/icons-material';
 import { makeStyles } from '@mui/styles';
+import {
+	Box,
+	Breadcrumbs,
+	Button,
+	Grid,
+	Link,
+	SvgIcon,
+	Typography,
+} from '@mui/material';
+import {
+	PlusCircle as PlusCircleIcon,
+	Download as DownloadIcon,
+	Upload as UploadIcon,
+} from 'react-feather';
+
+/*types definition */
+type Props = {
+	className?: string;
+};
 
 const useStyles = makeStyles(theme => ({
 	root: {},
@@ -11,13 +33,83 @@ const useStyles = makeStyles(theme => ({
 	},
 }));
 
-const Header = () => {
+const Header = ({ className, ...rest }: Props) => {
 	const classes = useStyles();
 
 	return (
-		<div>
-			<h1>Header - ListView - Works!</h1>
-		</div>
+		<Grid
+			container
+			justifyContent="space-between"
+			spacing={3}
+			className={clsx(classes.root, className)}
+			{...rest}
+		>
+			<Grid item>
+				<Breadcrumbs
+					separator={<NavigateNext fontSize="small" />}
+					aria-label="breadcrumb"
+				>
+					<Link
+						variant="body1"
+						color="inherit"
+						to="/dashboard"
+						component={RouterLink}
+					>
+						Dashboard
+					</Link>
+					<Link
+						variant="body1"
+						color="inherit"
+						underline="hover"
+						to="/dashboard/list-products"
+						component={RouterLink}
+					>
+						List Products
+					</Link>
+				</Breadcrumbs>
+				<Typography variant="h4" color="textPrimary">
+					All Products
+				</Typography>
+				<Box mt={2}>
+					<Button
+						className={classes.action}
+						startIcon={
+							<SvgIcon fontSize="small">
+								<UploadIcon />
+							</SvgIcon>
+						}
+					>
+						Import
+					</Button>
+					<Button
+						className={classes.action}
+						startIcon={
+							<SvgIcon fontSize="small">
+								<DownloadIcon />
+							</SvgIcon>
+						}
+					>
+						Export
+					</Button>
+				</Box>
+			</Grid>
+			<Grid item>
+				<Button
+					color="primary"
+					variant="contained"
+					className={classes.action}
+					component={RouterLink}
+					to="/dashboard/create-product"
+					startIcon={
+						<SvgIcon fontSize="small">
+							<PlusCircleIcon />
+						</SvgIcon>
+					}
+				>
+					New Product
+				</Button>
+			</Grid>
+		</Grid>
 	);
 };
 
