b53173b3d2c2f167c4e37ad787ff72aaa79e238f
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-10-25 17:57:46 +0200

    Update entry file

--- src/index.tsx
+++ src/index.tsx
@@ -30,19 +30,28 @@ import './locales/i18n';
 const store = configureAppStore();
 const MOUNT_NODE = document.getElementById('root') as HTMLElement;
 
-ReactDOM.render(
+interface Props {
+	Component: typeof App;
+}
+
+const ConnectedApp = ({ Component }: Props) => (
 	<Provider store={store}>
 		<HelmetProvider>
-			<App />
+			<Component />
 		</HelmetProvider>
-	</Provider>,
-	MOUNT_NODE,
+	</Provider>
 );
+
+const render = (Component: typeof App) => {
+	ReactDOM.render(<ConnectedApp Component={Component} />, MOUNT_NODE);
+};
 
 // Hot reloadable translation json files
 if (module.hot) {
-	module.hot.accept(['./locales/i18n'], () => {
-		// No need to render the App again because i18next works with the hooks
+	module.hot.accept(['./app', './locales/i18n'], () => {
+		ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+		const App = require('./app').App;
+		render(App);
 	});
 }
 
